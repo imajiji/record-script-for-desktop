0ddb7b44ff139b15992a82be1ae21fd39813a5d5
imaji <t.imajima.w2c@gmail.com>
2016-01-22 02:00:00 +0900

    changed logic

--- record-desktop-upgrade.sh
+++ record-desktop-upgrade.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]; then
+if [ $# -eq 2 ]; then
   urllist=$1
+  target_string=$2
+  recording_flg=`pgrep -fa recordmydesktop | wc -l`
+  chromium_flg=`pgrep -fa chromium-browse | wc -l`
 else
   echo "Not enough args."
   exit 1
 fi
 
-cat $urllist | while read url; do
-  echo $url
-done
+while read url; do
+  act_flg=`wget -q -O - $url | grep $target_string | wc -l`
+
+  if [ 0 -lt $act_flg -a 0 = $recording_flg ]; then
+
+    chromium-browser --add $url &
+    sleep 10s
+    recordmydesktop --width 550 --height 550 -y 200 -o /home/imaji/record/`date +'%Y%m%d_%H_%M_%S'` &
+
+    break
+  fi
+
+  if [ 0 -lt $act_flg -a 0 -lt $recording_flg ]; then
+    break
+  fi
+done < $urllist
+
+if [ 0 = $act_flg -a 0 -lt $recording_flg -a 0 -lt $chromium_flg ]; then
+  pkill --signal=SIGINT recordmydesktop
+  killall chromium-browse
+fi
