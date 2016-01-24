acc4527186bf9d28827a1e29672245285f5a76cb
imaji <t.imajima.w2c@gmail.com>
2016-01-24 23:45:23 +0900

    improved if & var

--- record-desktop-upgrade.sh
+++ record-desktop-upgrade.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+width=550
+height=550
+coordinate_y=200
+
 while true
 do
-  if [ $# -eq 2 ]; then
-    urllist=$1
-    target_string=$2
-    recording_flg=`pgrep -fa recordmydesktop | wc -l`
-    chromium_flg=`pgrep -fa chromium-browse | wc -l`
-  else
+  if [ $# -ne 2 ]; then
     echo "Not enough args."
     exit 1
   fi
+
+  urllist=$1
+  target_string=$2
+  recording_flg=`pgrep -fa recordmydesktop | wc -l`
+  chromium_flg=`pgrep -fa chromium-browse | wc -l`
 
   while read url; do
     act_flg=`wget -q -O - $url | grep $target_string | wc -l`
 
     if [ 0 -lt $act_flg -a 0 = $recording_flg ]; then
-
       chromium-browser --add $url &
       sleep 10s
-      recordmydesktop --width 550 --height 550 -y 200 -o /home/imaji/record/`date +'%Y%m%d_%H_%M_%S'` &
-
+      recordmydesktop --width $width --height $height -y $coordinate_y -o /home/imaji/record/`date +'%Y%m%d_%H_%M_%S'` &
       break
     fi
 
