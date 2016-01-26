89f4832c921460e0008d114019dbb148b8459e9b
imaji <t.imajima.w2c@gmail.com>
2016-01-26 23:47:24 +0900

    crean

--- record-desktop-upgrade.sh
+++ record-desktop-upgrade.sh
@@ -6,10 +6,7 @@ coordinate_y=200
 
 while true
 do
-  if [ $# -ne 2 ]; then
-    echo "Not enough args."
-    exit 1
-  fi
+  [ $# -ne 2 ] && echo "Not enough args."; exit 1
 
   urllist=$1
   target_string=$2
@@ -17,23 +14,23 @@ do
   chromium_flg=`pgrep -fa chromium-browse | wc -l`
 
   while read url; do
-    act_flg=`wget -q -O - $url | grep $target_string | wc -l`
+    act_flg=`wget -q -O - "$url" | grep "$target_string" | wc -l`
 
-    if [ 0 -lt $act_flg -a 0 = $recording_flg ]; then
+    if [ 0 -lt "$act_flg" -a 0 = "$recording_flg" ]; then
       chromium-browser --add $url &
       sleep 10s
       recordmydesktop --width $width --height $height -y $coordinate_y -o /home/imaji/record/`date +'%Y%m%d_%H_%M_%S'` &
       break
     fi
 
-    if [ 0 -lt $act_flg -a 0 -lt $recording_flg ]; then
+    if [ 0 -lt "$act_flg" -a 0 -lt "$recording_flg" ]; then
       break
     fi
   done < $urllist
 
-  if [ 0 = $act_flg -a 0 -lt $recording_flg -a 0 -lt $chromium_flg ]; then
-    pkill --signal=SIGINT recordmydesktop
-    killall chromium-browse
+  if [ 0 = "$act_flg" -a 0 -lt "$recording_flg" -a 0 -lt "$chromium_flg" ]; then
+    pkill -f --signal=SIGINT recordmydesktop
+    pkill -f chromium-browse
   fi
   sleep 5m
 done
